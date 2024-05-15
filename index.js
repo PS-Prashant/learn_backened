68bf6f5f7713f58cc6e915ef9f0b040cd4e0eeb6
unknown <prashant.sharma@tecdune.com>
2024-05-15 17:49:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+// import 'dotenv/config'
+
+const express = require('express')  
+// import express from 'express'
+
+const app = express()
+
+// const port = 3000 || 4000
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get( '/twitter', ( req, res ) =>  {
+	res.send( 'Prashant Sharma' )
+} )
+
+app.get( '/login', ( req, res ) => {
+	res.send( '<h1>Please Login</h1>' )
+} )
+
+app.get( '/test', ( req, res ) => {
+	res.json( { bukg: 'fhsadf'} )
+} )
+
+app.listen( port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
